03168a72819392f599cf2a4ceb7641bafca70ab2
hunghx <baby10051999@gmail.com>
2026-04-29 20:55:38 +0700

    ss5

--- order-service-eureka/src/main/java/re/orderservice/controller/ProductController.java
+++ order-service-eureka/src/main/java/re/orderservice/controller/ProductController.java
@@ -2,6 +2,7 @@ package re.orderservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ import java.util.List;
 @RequestMapping("/api/v2/products")
 public class ProductController {
     @GetMapping
-    public ResponseEntity<List<String>> getAllOrders() {
+    public ResponseEntity<List<String>> getAllOrders(@RequestHeader("token")String token) {
+        System.out.println("token:"+token);
         return ResponseEntity.ok().body(
                 Arrays.asList(new String[]{"Sản phần 1", "Sản phần 2", "Sản phần 3"})
         );
